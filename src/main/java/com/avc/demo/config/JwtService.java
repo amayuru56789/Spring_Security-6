bb1865ce1dd7aa8165e7692965ef91b97fce2de2
Amayuru Indeewara <amayuru_i@epic.lk>
2024-08-03 13:41:40 +0530

    added jwt-authentication filter

---
+++ src/main/java/com/avc/demo/config/JwtService.java
@@ -0,0 +1,30 @@
+package com.avc.demo.config;
+
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import org.springframework.stereotype.Service;
+
+import java.security.Key;
+
+@Service
+public class JwtService {
+
+    private static final String SECRET_KEY = "404E635266556A586E3272357538782F413F4428472B4B6250645367566B5970";
+
+    public String extractUsername(String token) {
+        return null;
+    }
+
+    private Claims extractAllClaims(String token) {
+        return Jwts
+                .parserBuilder()
+                .setSigningKey(getSignInKey())
+                .build()
+                .parseClaimsJws(token)
+                .getBody();
+    }
+
+    private Key getSignInKey() {
+        return null;
+    }
+}
